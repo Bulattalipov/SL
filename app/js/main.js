81b1da6a7948dcf95628a2b82d39105df0cdae92
Bulat <bulat0talipov@gmail.com>
2022-02-12 20:20:58 +0300

    added completed mane page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$ (function (){
-  
+
+let menuBtn = document.querySelector(".header__menu-btn");
+let navbar = document.querySelector(".navbar");
+
+menuBtn.addEventListener('click', function(){
+  menuBtn.classList.toggle("header__menu-btn--active");
+  navbar.classList.toggle("navbar__active");
 });
